2ee6419a1454ee732bc5500f9bc169c5257d980a
ule270 <u.le270@gmail.com>
2023-04-10 21:45:52 -0400

    exception for not equal length

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,7 +1,14 @@
 package zipcode;
 
 public class Hamming {
-    public Hamming(String s, String s1) {
+    private String left;
+    private String right;
+    public Hamming(String left, String right) {
+        if (left.length() != right.length()) {
+            throw new IllegalArgumentException(("leftStrand and rightStrand must be of equal length."));
+        }
+        this.left = left;
+        this.right = right;
     }
 
     public int getHammingDistance() {
